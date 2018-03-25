997a22d1f0ebeb9f526f5deb1a283b6ae992150a
Seth Watson <seth.watson1@gmail.com>
2018-03-25 09:01:49 -0700

    created Message component

---
+++ src/containers/AddMessage.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+const AddMessage = (props) => {
+    let input
+
+    return (
+        <section id="new-message"
+            <input
+            onKeyPress={(e) => {
+                if (e.key === 'Enter') {
+                    props.dispatch(input.value, 'Me')
+                    input.value = ''
+                }
+            }}
+            type="text"
+            ref={(node) => { 
+                input = node
+            }} />
+            </section>
+    )  
+}
+
+AddMessage.propTypes = {
+    dispatch: Proptypes.func.isRequired
+}
+
+export default AddMessage;
