164175beaeddd7aedb6c6fa547d8b425588f2eee
Seth Watson <seth.watson1@gmail.com>
2018-03-25 13:37:58 -0700

    installed redux-saga

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+
 import { addUser } from './actions';
 import { setupSocket } from './sockets'
 import chat from './reducers';
+import handleNewMessage from './sagas';
+import username from '.utils/name';
 
-const store = createStore(chat);
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers);
 
 store.dispatch(addUser('Me'))
 ReactDOM.render(
